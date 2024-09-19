590d2d8ac6ab23b87f1c577afd0aa3eae4570744
Thomas McLennan <exac@users.noreply.github.com>
2024-09-19 10:25:00 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'check-for-pre-transform-error-19.0.0-next.7';
+}
